b381f968402de798c9ea21d87cc7569063e5c182
Etienne de Villers <edevillers@users.noreply.github.com>
2022-06-28 23:25:04 -0400

    fix cursor

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if st.button('Get Fruit Load List'):
 # st.stop()
 
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('from streamlit')")
         return "Thanks for adding " + new_fruit
 
