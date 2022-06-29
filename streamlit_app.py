8f547837b67dd8b6636a742a43d3852ae42cf3dd
Etienne de Villers <edevillers@users.noreply.github.com>
2022-06-28 23:00:08 -0400

    ajout try-except

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,18 @@ st.dataframe(fruits_to_show)
 
 
 st.header("Fruityvice Fruit Advice !")
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered', fruit_choice)
+try:
+    fruit_choice = st.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        st.error("Please select a fruits to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+        st.dataframe(fruityvice_normalized)
 
+except URLError as e:
+    st.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-#  normalise le fruityvice
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# et l'affiche come df
-st.dataframe(fruityvice_normalized)
 
 # pour regarder une section à la fois
 st.stop()
